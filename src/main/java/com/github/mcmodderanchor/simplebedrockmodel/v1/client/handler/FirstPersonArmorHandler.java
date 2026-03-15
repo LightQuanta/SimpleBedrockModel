CrawKatt/SimpleBedrockModelReFabricated

盔甲和实体渲染示例；盔甲第一人称和史诗战斗兼容

src/main/java/com/github/mcmodderanchor/simplebedrockmodel/v1/client/handler/FirstPersonArmorHandler.java
@@ -1,0 +1,86 @@
+package com.github.mcmodderanchor.simplebedrockmodel.v1.client.handler;
+
+import com.github.mcmodderanchor.simplebedrockmodel.v1.client.renderer.GeoArmorRenderer;
+import com.github.mcmodderanchor.simplebedrockmodel.v1.common.model.BedrockBone;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderArmEvent;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Matrix4f;
+
+import java.util.ArrayDeque;
+
+/**
+ * 通用的第一人称盔甲手臂渲染处理器。
+ * 监听 RenderArmEvent，在玩家手臂上叠加渲染 Bedrock 盔甲模型的手臂部分。
+ */
+@Mod.EventBusSubscriber(Dist.CLIENT)
+public class FirstPersonArmorHandler {
+
+    private static HumanoidModel<?> defaultModel;
+
+    private static HumanoidModel<?> getDefaultModel() {
+        if (defaultModel == null) {
+            defaultModel = new HumanoidModel<>(
+                    Minecraft.getInstance().getEntityModels().bakeLayer(ModelLayers.PLAYER_INNER_ARMOR)
+            );
+        }
+        return defaultModel;
+    }
+
+    @SubscribeEvent
+    public static void onRenderArm(RenderArmEvent event) {
+        AbstractClientPlayer player = event.getPlayer();
+        HumanoidArm arm = event.getArm();
+
+        ItemStack chestStack = player.getItemBySlot(EquipmentSlot.CHEST);
+        if (chestStack.isEmpty()) return;
+
+        IClientItemExtensions ext = IClientItemExtensions.of(chestStack.getItem());
+        var model = ext.getHumanoidArmorModel(player, chestStack, EquipmentSlot.CHEST, getDefaultModel());
+        if (!(model instanceof GeoArmorRenderer geoRenderer)) return;
+
+        BedrockBone armBone = arm == HumanoidArm.RIGHT
+                ? geoRenderer.getModel().getArmorRightArm()
+                : geoRenderer.getModel().getArmorLeftArm();
+        if (armBone == null) return;
+
+        RenderType renderType = geoRenderer.getRenderType(geoRenderer.getTexture());
+        VertexConsumer consumer = event.getMultiBufferSource().getBuffer(renderType);
+
+        PoseStack poseStack = event.getPoseStack();
+        poseStack.pushPose();
+
+        poseStack.mulPoseMatrix(getGlobalTransform(armBone));
+
+        armBone.render(poseStack, consumer, event.getPackedLight(), OverlayTexture.NO_OVERLAY);
+
+        poseStack.popPose();
+    }
+
+    // 取得骨骼除了自身变换以外的全局变换矩阵
+    public static Matrix4f getGlobalTransform(@NotNull BedrockBone targetBone) {
+        Matrix4f matrix = new Matrix4f();
+
+        for (BedrockBone bone = targetBone.parent; bone != null; bone = bone.parent) {
+            matrix.scaleLocal(bone.xScale, bone.yScale, bone.zScale);
+            matrix.rotateLocal(bone.rotation);
+            matrix.translateLocal(bone.x / 16.0F, bone.y / 16.0F, bone.z / 16.0F);
+        }
+
+        return matrix;
+    }
+}

src/main/java/example/init/ExampleModRegister.java
@@ -2,14 +2,22 @@ package example.init;
 
 import example.block.TestBlock;
 import example.block.blockentity.TestBlockEntity;
+import example.entity.Zti;
+import example.item.ExampleArmorItem;
 import example.item.DeagleItem;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.common.ForgeSpawnEggItem;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -25,8 +33,16 @@ public class ExampleModRegister {
 
     public static Block TEST_BLOCK;
     public static BlockEntityType<TestBlockEntity> TEST_BLOCK_ENTITY_TYPE;
+    public static EntityType<Zti> ZTI_ENTITY_TYPE;
     public static BlockItem TEST_BLOCK_ITEM;
     public static DeagleItem DEAGLE_ITEM;
+
+    public static ExampleArmorItem DEFENDER_ARMOR_HELMET;
+    public static ExampleArmorItem DEFENDER_ARMOR_CHESTPLATE;
+    public static ExampleArmorItem DEFENDER_ARMOR_LEGGINGS;
+    public static ExampleArmorItem DEFENDER_ARMOR_BOOTS;
+
+    public static ForgeSpawnEggItem ZTI_SPAWN_EGG;
     public static CreativeModeTab TEST_TAB;
 
     @SubscribeEvent
@@ -43,11 +59,30 @@ public class ExampleModRegister {
             event.register(ForgeRegistries.BLOCK_ENTITY_TYPES.getRegistryKey(), modLoc("test_block_entity_type"), () -> TEST_BLOCK_ENTITY_TYPE);
         }
 
+        if (ForgeRegistries.ENTITY_TYPES.equals(registry)) {
+            ZTI_ENTITY_TYPE = EntityType.Builder.of(Zti::new, MobCategory.MONSTER)
+                    .sized(2.2F, 2.5F)
+                    .build(modLoc("zti").toString());
+            event.register(ForgeRegistries.ENTITY_TYPES.getRegistryKey(), modLoc("zti"), () -> ZTI_ENTITY_TYPE);
+        }
+
         if (ForgeRegistries.ITEMS.equals(registry)) {
             TEST_BLOCK_ITEM = new BlockItem(TEST_BLOCK, new BlockItem.Properties());
             DEAGLE_ITEM = new DeagleItem();
+            DEFENDER_ARMOR_HELMET = new ExampleArmorItem(ArmorItem.Type.HELMET);
+            DEFENDER_ARMOR_CHESTPLATE = new ExampleArmorItem(ArmorItem.Type.CHESTPLATE);
+            DEFENDER_ARMOR_LEGGINGS = new ExampleArmorItem(ArmorItem.Type.LEGGINGS);
+            DEFENDER_ARMOR_BOOTS = new ExampleArmorItem(ArmorItem.Type.BOOTS);
+            ZTI_SPAWN_EGG = new ForgeSpawnEggItem(() -> ZTI_ENTITY_TYPE, 0x61554D, 0xD8B076, new Item.Properties());
             event.register(ForgeRegistries.ITEMS.getRegistryKey(), modLoc("test_block_item"), () -> TEST_BLOCK_ITEM);
             event.register(ForgeRegistries.ITEMS.getRegistryKey(), modLoc("deagle"), () -> DEAGLE_ITEM);
+
+            event.register(ForgeRegistries.ITEMS.getRegistryKey(), modLoc("defender_helmet"), () -> DEFENDER_ARMOR_HELMET);
+            event.register(ForgeRegistries.ITEMS.getRegistryKey(), modLoc("defender_chestplate"), () -> DEFENDER_ARMOR_CHESTPLATE);
+            event.register(ForgeRegistries.ITEMS.getRegistryKey(), modLoc("defender_leggings"), () -> DEFENDER_ARMOR_LEGGINGS);
+            event.register(ForgeRegistries.ITEMS.getRegistryKey(), modLoc("defender_boots"), () -> DEFENDER_ARMOR_BOOTS);
+
+            event.register(ForgeRegistries.ITEMS.getRegistryKey(), modLoc("zti_spawn_egg"), () -> ZTI_SPAWN_EGG);
         }
 
         if (Registries.CREATIVE_MODE_TAB.equals(event.getRegistryKey())) {
@@ -56,8 +91,20 @@ public class ExampleModRegister {
                     .displayItems((parameters, output) -> {
                         output.accept(TEST_BLOCK_ITEM);
                         output.accept(DEAGLE_ITEM);
+                        output.accept(DEFENDER_ARMOR_HELMET);
+                        output.accept(DEFENDER_ARMOR_CHESTPLATE);
+                        output.accept(DEFENDER_ARMOR_LEGGINGS);
+                        output.accept(DEFENDER_ARMOR_BOOTS);
+                        output.accept(ZTI_SPAWN_EGG);
                     }).build();
             event.register(Registries.CREATIVE_MODE_TAB, modLoc("test_tab"), () -> TEST_TAB);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
+        if (ZTI_ENTITY_TYPE != null) {
+            event.put(ZTI_ENTITY_TYPE, Zti.createAttributes().build());
         }
     }
 

LightQuanta/SimpleBedrockModel

src/main/java/com/github/mcmodderanchor/simplebedrockmodel/v1/client/handler/FirstPersonArmorHandler.java
@@ -1,0 +1,84 @@
+package com.github.mcmodderanchor.simplebedrockmodel.v1.client.handler;
+
+import com.github.mcmodderanchor.simplebedrockmodel.v1.client.renderer.GeoArmorRenderer;
+import com.github.mcmodderanchor.simplebedrockmodel.v1.common.model.BedrockBone;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RenderArmEvent;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Matrix4f;
+
+/**
+ * 通用的第一人称盔甲手臂渲染处理器。
+ * 监听 RenderArmEvent，在玩家手臂上叠加渲染 Bedrock 盔甲模型的手臂部分。
+ */
+@EventBusSubscriber(Dist.CLIENT)
+public class FirstPersonArmorHandler {
+
+    private static HumanoidModel<?> defaultModel;
+
+    private static HumanoidModel<?> getDefaultModel() {
+        if (defaultModel == null) {
+            defaultModel = new HumanoidModel<>(
+                    Minecraft.getInstance().getEntityModels().bakeLayer(ModelLayers.PLAYER_INNER_ARMOR)
+            );
+        }
+        return defaultModel;
+    }
+
+    @SubscribeEvent
+    public static void onRenderArm(RenderArmEvent event) {
+        AbstractClientPlayer player = event.getPlayer();
+        HumanoidArm arm = event.getArm();
+
+        ItemStack chestStack = player.getItemBySlot(EquipmentSlot.CHEST);
+        if (chestStack.isEmpty()) return;
+
+        IClientItemExtensions ext = IClientItemExtensions.of(chestStack.getItem());
+        var model = ext.getHumanoidArmorModel(player, chestStack, EquipmentSlot.CHEST, getDefaultModel());
+        if (!(model instanceof GeoArmorRenderer geoRenderer)) return;
+
+        BedrockBone armBone = arm == HumanoidArm.RIGHT
+                ? geoRenderer.getModel().getArmorRightArm()
+                : geoRenderer.getModel().getArmorLeftArm();
+        if (armBone == null) return;
+
+        RenderType renderType = geoRenderer.getRenderType(geoRenderer.getTexture());
+        VertexConsumer consumer = event.getMultiBufferSource().getBuffer(renderType);
+
+        PoseStack poseStack = event.getPoseStack();
+        poseStack.pushPose();
+
+        poseStack.mulPose(getGlobalTransform(armBone));
+
+        armBone.render(poseStack, consumer, event.getPackedLight(), OverlayTexture.NO_OVERLAY);
+
+        poseStack.popPose();
+    }
+
+    // 取得骨骼除了自身变换以外的全局变换矩阵
+    public static Matrix4f getGlobalTransform(@NotNull BedrockBone targetBone) {
+        Matrix4f matrix = new Matrix4f();
+
+        for (BedrockBone bone = targetBone.parent; bone != null; bone = bone.parent) {
+            matrix.scaleLocal(bone.xScale, bone.yScale, bone.zScale);
+            matrix.rotateLocal(bone.rotation);
+            matrix.translateLocal(bone.x / 16.0F, bone.y / 16.0F, bone.z / 16.0F);
+        }
+
+        return matrix;
+    }
+}

src/main/java/example/init/ExampleModRegister.java
@@ -1,18 +1,26 @@
 package example.init;
 
-import example.animation.FPGunAnimationInstance;
 import example.capability.FPGunAnimationCapability;
+import example.entity.Zti;
 import example.item.DeagleItem;
+import example.item.ExampleArmorItem;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SpawnEggItem;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.attachment.AttachmentType;
+import net.neoforged.neoforge.common.DeferredSpawnEggItem;
+import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.neoforged.neoforge.registries.RegisterEvent;
-
-import java.util.function.Supplier;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public class ExampleModRegister {
@@ -21,31 +29,90 @@ public class ExampleModRegister {
      */
     public static final String MOD_ID = "example";
 
+    // public static Block TEST_BLOCK;
+    // public static BlockEntityType<TestBlockEntity> TEST_BLOCK_ENTITY_TYPE;
+    public static EntityType<Zti> ZTI_ENTITY_TYPE;
+//    public static BlockItem TEST_BLOCK_ITEM;
     public static Item DEAGLE_ITEM;
     public static AttachmentType<FPGunAnimationCapability> FP_GUN_ANIMATION;
 
+    public static ExampleArmorItem DEFENDER_ARMOR_HELMET;
+    public static ExampleArmorItem DEFENDER_ARMOR_CHESTPLATE;
+    public static ExampleArmorItem DEFENDER_ARMOR_LEGGINGS;
+    public static ExampleArmorItem DEFENDER_ARMOR_BOOTS;
+
+    public static SpawnEggItem ZTI_SPAWN_EGG;
+    public static CreativeModeTab TEST_TAB;
+
     @SubscribeEvent // on the mod event bus
     public static void register(RegisterEvent event) {
-        if (event.getRegistryKey().equals(Registries.ITEM)) {
-            DEAGLE_ITEM = new DeagleItem();
-            event.register(
-                    Registries.ITEM,
-                    registry -> {
-                        registry.register(modLoc("deagle"), DEAGLE_ITEM);
-                    }
-            );
-        } else if (event.getRegistryKey().equals(NeoForgeRegistries.ATTACHMENT_TYPES.key())) {
+        var registry = event.getRegistry();
+
+        if (event.getRegistryKey().equals(NeoForgeRegistries.ATTACHMENT_TYPES.key())) {
             FP_GUN_ANIMATION = AttachmentType.builder(FPGunAnimationCapability::new).build();
             event.register(
                     NeoForgeRegistries.ATTACHMENT_TYPES.key(),
-                    registry -> {
-                        registry.register(modLoc("fp_gun_ani"), FP_GUN_ANIMATION);
-                    }
+                    r -> r.register(modLoc("fp_gun_ani"), FP_GUN_ANIMATION)
             );
         }
 
+//        if (BuiltInRegistries.BLOCK.equals(registry)) {
+//            TEST_BLOCK = new TestBlock();
+//            event.register(BuiltInRegistries.BLOCK.key(), modLoc("test_block"), () -> TEST_BLOCK);
+//        }
+//
+//        if (BuiltInRegistries.BLOCK_ENTITY_TYPE.equals(registry)) {
+//            TEST_BLOCK_ENTITY_TYPE = BlockEntityType.Builder.of(TestBlockEntity::new, TEST_BLOCK).build(null);
+//            event.register(BuiltInRegistries.BLOCK_ENTITY_TYPE.key(), modLoc("test_block_entity_type"), () -> TEST_BLOCK_ENTITY_TYPE);
+//        }
 
+        if (BuiltInRegistries.ENTITY_TYPE.equals(registry)) {
+            ZTI_ENTITY_TYPE = EntityType.Builder.of(Zti::new, MobCategory.MONSTER)
+                    .sized(2.2F, 2.5F)
+                    .build(modLoc("zti").toString());
+            event.register(BuiltInRegistries.ENTITY_TYPE.key(), modLoc("zti"), () -> ZTI_ENTITY_TYPE);
+        }
 
+        if (BuiltInRegistries.ITEM.equals(registry)) {
+//            TEST_BLOCK_ITEM = new BlockItem(TEST_BLOCK, new BlockItem.Properties());
+            DEAGLE_ITEM = new DeagleItem();
+            DEFENDER_ARMOR_HELMET = new ExampleArmorItem(ArmorItem.Type.HELMET);
+            DEFENDER_ARMOR_CHESTPLATE = new ExampleArmorItem(ArmorItem.Type.CHESTPLATE);
+            DEFENDER_ARMOR_LEGGINGS = new ExampleArmorItem(ArmorItem.Type.LEGGINGS);
+            DEFENDER_ARMOR_BOOTS = new ExampleArmorItem(ArmorItem.Type.BOOTS);
+            ZTI_SPAWN_EGG = new DeferredSpawnEggItem(() -> ZTI_ENTITY_TYPE, 0x61554D, 0xD8B076, new Item.Properties());
+//            event.register(BuiltInRegistries.ITEM.key(), modLoc("test_block_item"), () -> TEST_BLOCK_ITEM);
+            event.register(BuiltInRegistries.ITEM.key(), modLoc("deagle"), () -> DEAGLE_ITEM);
+
+            event.register(BuiltInRegistries.ITEM.key(), modLoc("defender_helmet"), () -> DEFENDER_ARMOR_HELMET);
+            event.register(BuiltInRegistries.ITEM.key(), modLoc("defender_chestplate"), () -> DEFENDER_ARMOR_CHESTPLATE);
+            event.register(BuiltInRegistries.ITEM.key(), modLoc("defender_leggings"), () -> DEFENDER_ARMOR_LEGGINGS);
+            event.register(BuiltInRegistries.ITEM.key(), modLoc("defender_boots"), () -> DEFENDER_ARMOR_BOOTS);
+
+            event.register(BuiltInRegistries.ITEM.key(), modLoc("zti_spawn_egg"), () -> ZTI_SPAWN_EGG);
+        }
+
+        if (Registries.CREATIVE_MODE_TAB.equals(event.getRegistryKey())) {
+            TEST_TAB = CreativeModeTab.builder().title(Component.translatable("item_group.example.name"))
+                    .icon(() -> DEAGLE_ITEM.getDefaultInstance())
+                    .displayItems((parameters, output) -> {
+//                        output.accept(TEST_BLOCK_ITEM);
+                        output.accept(DEAGLE_ITEM);
+                        output.accept(DEFENDER_ARMOR_HELMET);
+                        output.accept(DEFENDER_ARMOR_CHESTPLATE);
+                        output.accept(DEFENDER_ARMOR_LEGGINGS);
+                        output.accept(DEFENDER_ARMOR_BOOTS);
+                        output.accept(ZTI_SPAWN_EGG);
+                    }).build();
+            event.register(Registries.CREATIVE_MODE_TAB, modLoc("test_tab"), () -> TEST_TAB);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
+        if (ZTI_ENTITY_TYPE != null) {
+            event.put(ZTI_ENTITY_TYPE, Zti.createAttributes().build());
+        }
     }
 
     public static ResourceLocation modLoc(String name) {